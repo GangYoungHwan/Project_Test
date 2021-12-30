1ec35806579385fae4d5c0a1f98257cc95d8e80d
GangYoungHwan <fbnet@naver.com>
2021-12-30 13:46:12 +0900

    1-2test

---
+++ Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform _target;
+    public float _distance = 5.0f;
+    public float heightoffset = 3.0f;
+    public float cameraDelay = 0.02f;
+
+    void Update()
+    {
+        Vector3 followPos = _target.position - _target.forward * _distance;
+
+        followPos.y += heightoffset;
+        transform.position += (followPos - transform.position) * cameraDelay;
+
+        transform.LookAt(_target.transform);
+    }
+}
