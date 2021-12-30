61bd7a2938d50cb0e6ecf1ae8d67bf7c27f19468
GangYoungHwan <fbnet@naver.com>
2021-12-30 14:52:36 +0900

    Depence12-30

---
+++ Defence/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public Transform _taget;
+    NavMeshAgent _agent;
+    Animator _animator;
+
+    void Start()
+    {
+        _agent = this.GetComponent<NavMeshAgent>();
+        _animator = this.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        _agent.SetDestination(_taget.position);
+        _animator.SetFloat("MoveSpeed", 1.0f);
+    }
+}
