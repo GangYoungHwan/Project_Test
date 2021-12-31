3cbb836f23aa40195af36ae0c33e8d709e373170
GangYoungHwan <fbnet@naver.com>
2021-12-31 14:41:33 +0900

    12/31

---
+++ Defence/Assets/Scripts/Tower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    int Tower_HP;
+    int Tower_def;
+    void Start()
+    {
+        Tower_HP = 3000;
+        Tower_def = 10;
+    }
+
+    void Update()
+    {
+        
+    }
+}
