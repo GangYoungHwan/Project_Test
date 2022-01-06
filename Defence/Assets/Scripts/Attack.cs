9e85417fcb39e5082ef28f01d7bf7b82430b1be8
GangYoungHwan <fbnet@naver.com>
2022-01-07 00:58:33 +0900

    1.06.1

--- Defence/Assets/Scripts/Attack.cs
+++ Defence/Assets/Scripts/Attack.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    GameObject _taget;
+    public float _Distance = 4.0f;
+    float dis;
     void Start()
     {
-        
+        _taget = GameObject.Find("Tower");
     }
 
     void Update()
     {
-        
+        dis = Vector3.Distance(_taget.transform.position, this.transform.position);
+        if(dis< _Distance)
+        {
+            Debug.Log("공격중");
+        }
     }
 }
