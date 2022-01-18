d7dbd5129a2a269658da868093cd9585ecbbd222
GangYoungHwan <fbnet@naver.com>
2022-01-18 14:39:37 +0900

    01.18.00

---
+++ Defence/Assets/Defence/Scripts/UI/CardUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardUI : MonoBehaviour
+{
+    public Camera _camerapos;
+    bool _cardOn;
+    void Start()
+    {
+        _cardOn = false;
+    }
+
+
+    void Update()
+    {
+        
+    }
+    public void Card()
+    {
+        if (_cardOn)
+        {
+            this.gameObject.SetActive(false);
+            _camerapos.transform.position = new Vector3(36, 35, 4);
+            _cardOn = false;
+        }
+        else
+        {
+            this.gameObject.SetActive(true);
+            _camerapos.transform.position = new Vector3(44, 35, 12);
+            _cardOn = true;
+        }
+    }
+
+    public void CardSpwan()
+    {
+
+    }
+}
