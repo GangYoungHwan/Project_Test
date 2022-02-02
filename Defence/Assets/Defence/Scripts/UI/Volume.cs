8e777354eaeae8b6c570d3080dd008b937be100a
GangYoungHwan <fbnet@naver.com>
2022-02-03 01:43:36 +0900

    02.03.00

---
+++ Defence/Assets/Defence/Scripts/UI/Volume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    public Slider _volume;
+    Text _volumeText;
+    void Start()
+    {
+        _volumeText = GetComponent<Text>();
+        _volume.value = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int Vol = (int)_volume.value;
+        _volumeText.text = Vol.ToString()+"%";
+    }
+}
